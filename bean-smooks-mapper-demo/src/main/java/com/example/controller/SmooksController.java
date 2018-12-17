7358bcac514e606d88249219c7f900840ccf36f2
githubshenyuan <35088383+githubshenyuan@users.noreply.github.com>
2018-12-17 19:30:27 +0800

    Initial commit

---
+++ bean-smooks-mapper-demo/src/main/java/com/example/controller/SmooksController.java
@@ -0,0 +1,8 @@
+package com.example.controller;
+
+/**
+ * @author sh 2018/12/17 19:28
+ * @version ideaIU-2018.2.5.win
+ */
+public class SmooksController {
+}
---
+++ bean-tamper-mapper-demo/src/main/java/com/example/controller/TamperController.java
@@ -0,0 +1,9 @@
+package com.example.controller;
+
+/**
+ * @author sh 2018/12/17 19:29
+ * @version ideaIU-2018.2.5.win
+ */
+public class TamperController {
+
+}
